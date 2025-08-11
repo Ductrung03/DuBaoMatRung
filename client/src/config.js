c4c1d8e9994ef6db0708483637224e172513d77f
Ductrung03 <Ductrung021103@gmail.com>
2025-08-11 01:50:15 +0000

    a

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: import.meta.env.VITE_API_URL || "http://localhost:3000"
+  API_URL: import.meta.env.VITE_API_URL || "http://localhost:3000" ||  "http://103.57.223.237:3000"
 };
 
 // Log để debug
